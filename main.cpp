a7e67a3548992465043fc2dead5385d1e4d78f59
tostoner <95102280+tostoner@users.noreply.github.com>
2022-04-04 19:47:59 +0200

    changed from std::cin to getline

--- main.cpp
+++ main.cpp
@@ -4,16 +4,12 @@
 
 #include <iostream>
 #include "myTree.h"
+#include <string>
 
 
 
-
-int main() {
-    int menu = 98;
-    int exit = 99;
-    int menuPointer = menu;
-
-
+int main()
+{
     std::cout << "Welcome to ancestor tree.\n\n"
                  "Type the root name: ";
     std::string name;
@@ -23,131 +19,7 @@ int main() {
     std::cout << "The root name is " << name << "." << std::endl;
     node currentNode = root;
 
+    main_menu(currentNode);
 
-    bool exitLoop = false;
-    while (!exitLoop) {
-
-        switch (menuPointer) {
-
-            case 1: { // change selected person
-                std::cout << "1. Select parent \n"
-                             "2. Select child.\n"
-                             "3. Go back to menu";
-                menuPointer = menuInput();
-                switch (menuPointer) {
-
-                    case 1: {
-                        if (currentNode.isRoot()) {
-                            std::cout << "Current node have no parents. select something else. \n";
-                            menuPointer = menuInput();
-                            currentNode = *currentNode.getParent();
-                        }
-                        else {
-                            currentNode = *currentNode.getParent();
-                        }
-                        break;
-                    }
-                    case 2: {
-                        if (currentNode.isLeaf()) {
-                            std::cout << "Current node has no children. select something else. \n";
-                            menuPointer = menuInput();
-                        }
-                        else {
-                            std::cout << "Choose one of the following numbers: \n";
-                            currentNode.printChildren();
-                            menuPointer = menuInput();
-                            currentNode = currentNode.getChildren()[menuPointer-1];
-                        }
-                        break;
-                    }
-                    case 3:
-                        menuPointer = menu;
-                        break;
-                    default:
-                        defaultMsg();
-                        menuPointer = menuInput();
-                }
-
-            }
-            case 2: { // print children
-                if(currentNode.isLeaf()){
-                    std::cout << "Current node have no children. select something else. \n";
-
-                }
-                else{
-                    currentNode.printChildren();
-                }
-                menuPointer = menu;
-
-                break;
-            }
-            case 3: { // print parent
-                if(currentNode.isRoot()){
-                    std::cout << "Current node have no parents. select something else. \n";
-                }
-                else{
-                    node previousNode("p");
-                    previousNode = *currentNode.getParent();
-                    std::cout<<previousNode.getName()<<std::endl;
-                }
-                menuPointer = menu;
-                break;
-            }
-            case 4: { //addChild
-                std::cout << "Type child name: " << std::endl;
-                std::getline(std::cin, name);
-                node n(name);
-                currentNode.addChild(n);
-            }
-
-
-            case 98: { //menu
-                std::cout << "Currently selected is " << currentNode.getName() << "." << std::endl;
-                std::cout << "1. Change selected person\n"
-                             "2. Get children\n"
-                             "3. Get parent\n"
-                             "4. Add child\n"
-                             "5. Exit program\n";
-                std::string s;
-                menuPointer = menuInput();
-
-
-                switch (menuPointer) {
-                    case 1:
-                        menuPointer  = menuPointer;
-                        break;
-                    case 2:
-                        menuPointer = menuPointer;
-                        break;
-                    case 3:
-                        menuPointer = menuPointer;
-                        break;
-                    case 4:
-                        menuPointer = menuPointer;
-                        break;
-                    case 5:
-                        menuPointer = exit;
-                        break;
-                    default:
-                        defaultMsg();
-                        menuPointer = menuInput();
-                }
-                break;
-            }
-
-            case 99: { //exit case
-                exitLoop = true;
-                std::cout << "Exiting program...";
-                break;
-            }
-            default: {
-
-                defaultMsg();
-                menuPointer = menuInput();
-                break;
-            }
-        }
-
-    }
-    return 1;
+    return 0;
 }
--- myTree.h
+++ myTree.h
@@ -52,17 +52,156 @@ private:
     node* parent_ = nullptr;
 };
 
-
-
-int menuInput(){
+int inputToInt(){
     std::string s;
     std::getline(std::cin, s);
-
-    return std::stoi(s);
+    return stoi(s);
 }
+
 void defaultMsg(){
     std::cout << "Invalid number. Try again: ";
 }
+void get_children(const node &currentNode)
+{
+    if (currentNode.isLeaf())
+    {
+        std::cout << "Current node have no children. select something else. \n";
+    }
+    else
+    {
+        currentNode.printChildren();
+    }
+}
+
+void select_parent(node &currentNode)
+{
+    int selection;
+    if (currentNode.isRoot())
+    {
+        std::cout << "Current node have no parents. select something else. \n";
+        selection = inputToInt();
+    }
+    else
+    {
+        currentNode = *currentNode.getParent();
+    }
+}
+
+void select_child(node &currentNode)
+{
+    int selection;
+    if (currentNode.isLeaf())
+    {
+        std::cout << "Current node has no children. select something else. \n";
+        selection = inputToInt();
+    }
+    else
+    {
+        std::cout << "Choose one of the following numbers: \n";
+        currentNode.printChildren();
+        selection = inputToInt();
+        currentNode = currentNode.getChildren()[selection + 1];
+    }
+}
+
+void select_person(node &currentNode)
+{
+    int selection;
+    while (true)
+    {
+
+        std::cout << "1. Select parent \n"
+                     "2. Select child.\n"
+                     "3. Go back to menu";
+        selection = inputToInt();
+        switch (selection)
+        {
+
+            case 1:
+            {
+                select_parent(currentNode);
+                break;
+            }
+            case 2:
+            {
+                select_child(currentNode);
+                break;
+            }
+            case 3:
+                return;
+            default:
+                defaultMsg();
+        }
+    }
+}
+
+void get_parent(node &currentNode)
+{
+    if (currentNode.isRoot())
+    {
+        std::cout << "Current node have no parents. select something else. \n";
+    }
+    else
+    {
+        std::cout << currentNode.getParent()->getName();
+    }
+}
+
+void add_child(node &currentNode)
+{
+    std::string name;
+    std::cout << "Type child name: " << std::endl;
+    std::getline(std::cin, name);
+    node n(name);
+    currentNode.addChild(n);
+}
+
+void main_menu(node &currentNode)
+{
+    int selection;
+
+    while (true)
+    {
+        std::cout << "Currently selected is " << currentNode.getName() << "." << std::endl;
+        std::cout << "1. Change selected person\n"
+                     "2. Get children\n"
+                     "3. Get parent\n"
+                     "4. Add child\n"
+                     "5. Exit program\n";
+        selection = inputToInt();
+
+        switch (selection)
+        {
+
+            case 1:
+            { // change selected person
+                select_person(currentNode);
+                break;
+            }
+            case 2:
+            {
+                get_children(currentNode);
+                break;
+            }
+            case 3:
+            {
+                get_parent(currentNode);
+                break;
+            }
+            case 4:
+            { // addChild
+                add_child(currentNode);
+                break;
+            }
+            case 5:
+                std::cout << "Exiting program...";
+                return;
+            default:
+                defaultMsg();
+        }
+    }
+}
+
 
 
 #endif //SLEKTSTRE_MYTREE_H
