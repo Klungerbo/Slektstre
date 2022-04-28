569c7da366cc06785c201ed9141a143704d43bcd
Torstein <torstein_skare@hotmail.com>
2022-04-28 15:47:31 +0200

    started fixing tests again..

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,40 +6,40 @@
 #include "catch.hpp"
 #include "../include/Node.hpp"
 
-node testNode("Pål pil", 20, node::Male);
+Person testPerson("Pål pil", 20, Person::Male);
 
 TEST_CASE("TEST getName") {
-    std::string nameAnswer = testNode.getName();
+    std::string nameAnswer = testPerson.getName();
     REQUIRE(nameAnswer == "Pål pil");
 }
 
 TEST_CASE("TEST getAge") {
-    REQUIRE(testNode.getAge() == 20);
+    REQUIRE(testPerson.getAge() == 20);
 
 }
 TEST_CASE("TEST getParent"){
-    node testNodeChild("testChild", 4, node::Female);
-    testNode.addChild(testNodeChild);
-    node parentNode = *testNodeChild.getParent();
-    REQUIRE(parentNode == testNode);
+    Person testChild("testChild", 4, Person::Female);
+    Node testNode(testPerson);
+
+    REQUIRE(parentNode == testPerson);
 }
 
 TEST_CASE("TEST getChildren") {
-    node testNodeChild("Pål Pil", 1, node::Female);
-    testNode.addChild(testNodeChild);
-    REQUIRE(testNode.getChildren()[0] == testNodeChild);
+    node testPersonChild("Pål Pil", 1, node::Female);
+    testPerson.addChild(testPersonChild);
+    REQUIRE(testPerson.getChildren()[0] == testPersonChild);
 
 }
 
 TEST_CASE("TEST getGender") {
-    int gender = testNode.getGender();
+    int gender = testPerson.getGender();
     REQUIRE(gender == node::Male);
 }
 
 TEST_CASE("TEST addChild") {
     node child("Petter Pil", 1, node::Male);
-    testNode.addChild(child);
-    REQUIRE(testNode.getChildren().size() == 1);
+    testPerson.addChild(child);
+    REQUIRE(testPerson.getChildren().size() == 1);
 }
 
 
