80149d0831e9500c870f127552b19c4dbb607e55
tostoner <95102280+tostoner@users.noreply.github.com>
2022-05-15 12:56:59 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Torstein/CLionProjects/Slektstre
-# Build directory: C:/Users/Torstein/CLionProjects/Slektstre/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tool")
-subdirs("tests")
